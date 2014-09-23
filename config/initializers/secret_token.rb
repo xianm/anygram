e95643b3f00b9738b74acf58cded36ed31100dbc
Christian Moniz <xianmoniz@gmail.com>
2014-09-23 09:52:10 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anygram::Application.config.secret_key_base = '5df269563125834d44043974f3ef6a6f3f772b761d57dc30c34cba36a61c231a12493bb4676e2269aae934f731d940b6ba989860896b0b752eeb3c6839a951ec'
